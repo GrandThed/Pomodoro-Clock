36e206c8b0efaa0d3950aae226d1b14ec2c3ec59
GranThed <benjamincanas1@gmail.com>
2020-08-21 17:56:04 -0300

    Updates

--- src/context/ChangingTitle.js
+++
@@ -1,7 +0,0 @@
-export const changingTitle = (seconds, working) => {
-    document.title = `${
-        Math.floor(seconds / 60) <= 9
-          ? "0" + Math.floor(seconds / 60)
-          : Math.floor(seconds / 60)
-      }:${seconds % 60 <= 9 ? "0" + (seconds % 60) : seconds % 60} ${working ? "of working" : "for a break!"}`
-}
--- src/context/notifications.js
+++
@@ -1,6 +0,0 @@
-export const notifications = (notificationState, text) => {
-    if (notificationState) {
-        const notification = new Notification(text)
-        setTimeout(notification.close(), 1000)
-    }
-}
